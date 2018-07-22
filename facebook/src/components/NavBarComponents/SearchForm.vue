00f9580c003ed56b327036d250492e7262346557
denzow <denzow@gmail.com>
2018-07-23 00:11:52 +0900

    [wip] serach box question icon

--- facebook/src/components/NavBarComponents/SearchForm.vue
+++ facebook/src/components/NavBarComponents/SearchForm.vue
@@ -1,12 +1,20 @@
 <template>
-  <span>
-    <input type="text" value=""  class="search-form" />
+  <span class="search-form">
+    <input type="text" value="" class="search-box"
+           @focus="isFocused = true"
+           @blur="isFocused = false"/>
+    <i class="fas fa-search" :class="{ focusedIcon: isFocused }"></i>
   </span>
 </template>
 
 <script>
 export default {
   name: 'SearchForm',
+  data() {
+    return {
+      isFocused: false,
+    };
+  }
 };
 </script>
 
@@ -14,5 +22,29 @@ export default {
 .search-form {
   width: 12rem;
   margin-right: 5rem;
+  background-color: white;
+  border-radius: 0.3rem;
+  padding-left: 0.5rem;
+  height: 1.5rem;
+}
+
+.search-box {
+  border: 0;
+  width: 85%;
+  outline: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.fas {
+  color: #42b983;
+  height: 100%;
+  font-size: 1rem;
+  width: 15%;
+}
+
+.focusedIcon {
+  background-color: #42b983;
+  color: white;
 }
 </style>
